f16e7f73ea195a8a665570b8587a657a72b8f1be
Haku <gmkohaku@gmail.com>
2019-08-27 23:38:42 +0800

    modify search & poe detail

---
+++ pages/search/index.js
@@ -0,0 +1,215 @@
+// pages/search/index.js
+wx.cloud.init()
+const db = wx.cloud.database()
+
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    hasMoreWorks: false,
+    hasMoreAuthors: false
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    this.setData({
+      inputShowed:true
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  naviback: function () {    
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      hasMoreWorks: false,
+      hasMoreAuthors: false,
+      searchResultWorks: '',
+      searchResultAuthors: '',
+    })
+
+    wx.showToast({
+      title: '数据加载中',
+      icon: 'loading',
+      duration: 100000
+    })
+
+    console.log(e.detail.value)
+    const _ = db.command
+    // db.collection("works_all").where(_.or([
+    //   {
+    //     Content: {
+    //       $regex: '.*' + e.detail.value,
+    //       $options: 'i'
+    //     }
+    //   },
+    //   {
+    //     Title: {
+    //       $regex: '.*' + e.detail.value,
+    //       $options: 'i'
+    //     }
+    //   }
+    // ])).orderBy('WorkId', 'asc').get({
+    //   success: res => {
+    //     console.log(res.data)
+    //     this.setData({
+    //       searchResultWorks: res.data
+    //     })
+
+    //     if(res.data == ''){
+    //       this.setData({
+    //         noResultWork: true
+    //       })
+    //     }
+    //     else{
+    //       this.setData({
+    //         noResultWork: false
+    //       })
+    //       if (res.data.length > 4) {
+    //         this.setData({
+    //           hasMoreWorks: true
+    //         })
+    //       }
+    //     }
+    //   }
+    // })
+
+    db.collection("works_all").where(
+      {
+        Content: {
+          $regex: '.*' + e.detail.value,
+          $options: 'i'
+        }
+      }).orderBy('WorkId', 'asc').get({
+        success: res => {
+          console.log('0000000000')
+          console.log(res.data)
+          this.setData({
+            searchResultWorks: res.data
+          })
+
+          if (res.data == '') {
+            console.log("works null")
+            this.setData({
+              noResultWork: true
+            })
+          }
+          else {
+            this.setData({
+              noResultWork: false
+            })
+            if (res.data.length > 4) {
+              this.setData({
+                hasMoreWorks: true
+              })
+            }
+          }
+        },
+        fail: err => {
+          console.error(err)
+        },
+        complete: () => {
+          wx.hideToast()
+        }
+      })
+
+    db.collection("authors_all").where({
+      authorname: {
+        $regex: '.*' + e.detail.value,
+        $options: 'i'
+      }
+    }).orderBy('authorid', 'asc').get({
+      success: res => {
+        this.setData({
+          searchResultAuthors: res.data
+        })
+
+        if (res.data == '') {
+          console.log("authors null")
+          this.setData({
+            noResultAuthor: true
+          })
+        }
+        else {
+          this.setData({
+            noResultAuthor: false
+          })
+          if (res.data.length > 4) {
+            this.setData({
+              hasMoreAuthors: true
+            })
+          }
+        }
+      }
+    })
+
+    this.setData({
+      inputVal: e.detail.value
+    });
+  }
+})
