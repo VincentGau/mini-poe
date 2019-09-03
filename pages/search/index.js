ae5c9162a648fcec131856cf54b2d285347f31ee
Haku <gmkohaku@gmail.com>
2019-09-03 23:11:15 +0800

    增加搜索历史记录

--- pages/search/index.js
+++ pages/search/index.js
@@ -8,18 +8,30 @@ Page({
    * Page initial data
    */
   data: {
-    hasMoreWorks: false,
-    hasMoreAuthors: false,
+    hasMoreWorks: false, //是否显示查看更多作品
+    hasMoreAuthors: false, //是否显示查看更多作者
     completed:false,
+    searchHistory:[], //搜索历史记录
+    hideSearchHistory:false,
+    hasSearchHistory:false,
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    var a = wx.getStorageSync('searchHistory') 
+    console.log('STORAGE') 
+    console.log(a)
     this.setData({
-      inputShowed:true
+      inputShowed:true,
+      searchHistory: wx.getStorageSync('searchHistory')  
     })
+    if (wx.getStorageSync('searchHistory')){
+      this.setData({
+        showClear:true,
+      })
+    }
   },
 
   /**
@@ -87,14 +99,29 @@ Page({
     });
   },
   inputTyping: function (e) {
+    var that = this
     this.setData({
       hasMoreWorks: false,
       hasMoreAuthors: false,
       searchResultWorks: '',
       searchResultAuthors: '',
       keyword :e.detail.value,
+      showClear:false,
+      hideSearchHistory:true,
     })
 
+    // 搜索后将搜索记录缓存到本地
+    if ("" != e.detail.value) {
+      var searchHistory = that.data.searchHistory;
+      if(searchHistory == ''){
+        searchHistory = []
+      }
+      // 使用队列，保证在查看搜索历史记录时最近搜索内容在顶部
+      searchHistory.unshift(e.detail.value);
+      wx.setStorageSync('searchHistory', searchHistory);
+    }
+
+    // 停止输入时显示加载中提示，查询完成后隐藏
     wx.showToast({
       title: '数据加载中',
       icon: 'loading',
@@ -189,7 +216,7 @@ Page({
     //     }
     //   })
 
-    db.collection("authors_all").where({
+    db.collection("authors").where({
       authorname: {
         $regex: '.*' + e.detail.value,
         $options: 'i'
@@ -225,5 +252,136 @@ Page({
     this.setData({
       inputVal: e.detail.value
     });
-  }
+  },
+
+  // 清除搜索历史记录
+  clearSearchHistory: function(){
+    wx.removeStorageSync('searchHistory')
+    this.setData({
+      hideSearchHistory:true,
+      showClear:false
+    })
+  },
+
+  doSearch:function(e){
+    var p = e.currentTarget.dataset.text
+    var that = this
+    this.setData({
+      hasMoreWorks: false,
+      hasMoreAuthors: false,
+      searchResultWorks: '',
+      searchResultAuthors: '',
+      keyword: p,
+      showClear: false,
+      hideSearchHistory: true,
+    })
+
+    // 搜索后将搜索记录缓存到本地
+    if ("" != p) {
+      var searchHistory = that.data.searchHistory;
+      if (searchHistory == '') {
+        searchHistory = []
+      }
+      // 使用队列，保证在查看搜索历史记录时最近搜索内容在顶部
+      searchHistory.unshift(p);
+      wx.setStorageSync('searchHistory', searchHistory);
+    }
+
+    // 停止输入时显示加载中提示，查询完成后隐藏
+    wx.showToast({
+      title: '数据加载中',
+      icon: 'loading',
+      duration: 100000
+    })
+
+    const _ = db.command
+
+    db.collection("works_hot").where(_.or([
+      {
+        Content: {
+          $regex: '.*' + p,
+          $options: 'i'
+        }
+      },
+      {
+        Title: {
+          $regex: '.*' + p,
+          $options: 'i'
+        }
+      }
+    ])).orderBy('LikesCount', 'desc').get({
+      success: res => {
+        this.setData({
+          searchResultWorks: res.data,
+          completed: true
+        })
+
+        wx.setStorage({
+          key: "works20",
+          data: res.data
+        })
+
+        if (res.data == '') {
+          this.setData({
+            noResultWork: true
+          })
+        }
+        else {
+          this.setData({
+            noResultWork: false
+          })
+          if (res.data.length > 4) {
+            this.setData({
+              hasMoreWorks: true
+            })
+          }
+        }
+      },
+      fail: err => {
+        console.error(err)
+      },
+      complete: () => {
+        wx.hideToast()
+      }
+    })
+
+    
+
+    db.collection("authors_all").where({
+      authorname: {
+        $regex: '.*' + p,
+        $options: 'i'
+      }
+    }).orderBy('authorid', 'asc').get({
+      success: res => {
+        wx.setStorage({
+          key: "authors20",
+          data: res.data
+        })
+        this.setData({
+          searchResultAuthors: res.data,
+        })
+
+        if (res.data == '') {
+          this.setData({
+            noResultAuthor: true
+          })
+        }
+        else {
+          this.setData({
+            noResultAuthor: false
+          })
+          if (res.data.length > 4) {
+            this.setData({
+              hasMoreAuthors: true
+            })
+          }
+        }
+      }
+    })
+
+    this.setData({
+      inputVal: p
+    });
+  },
 })
