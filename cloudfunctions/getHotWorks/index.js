cf8e98ef34e4d19c475cfe053603ad462c737e23
haku <g@kohaku.cc>
2021-01-03 13:15:11 +0800

    删除冗余

--- cloudfunctions/getHotWorks/index.js
+++ cloudfunctions/getHotWorks/index.js
@@ -11,12 +11,12 @@ const MAX_LIMIT = 100
 exports.main = async (event, context) => {
   // collection 上的 get 方法会返回一个 Promise，因此云函数会在数据库异步取完数据后返回结果
   console.log(event.startBatch + " - " + event.endBatch)
-  const countResult = await db.collection('works_hot').count()
-  console.log(countResult)
-  const total = countResult.total
+  // const countResult = await db.collection('works_hot').count()
+  // console.log(countResult)
+  // const total = countResult.total
   // 计算需分几次取
-  var batchTimes = Math.ceil(total / MAX_LIMIT)
-  batchTimes = 40 // 防止并发超过一定数量 Promise.all + reduce也算并发
+  // var batchTimes = Math.ceil(total / MAX_LIMIT)
+  // batchTimes = 40 // 防止并发超过一定数量 Promise.all + reduce也算并发
   // 承载所有读操作的 promise 的数组
   const tasks = []
   const _ = db.command
